ba4ac102e4d39dfe4fd01c0f2610f2de82a405f3
nikhitag79 <nguntu@gmail.com>
2023-03-25 17:25:36 -0400

    adding item page

--- mysite/BetterCart/urls.py
+++ mysite/BetterCart/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('grocerylist/', views.display_grocery, name = 'display grocery')
+    path('grocerylist/', views.display_grocery, name = 'display grocery'),
+    path('add-item/', views.addItemView, name='add-item'),
+    path('finalize-list/', views.finalizeListView, name='finalize-list'),
 ]
--- mysite/BetterCart/views.py
+++ mysite/BetterCart/views.py
@@ -14,18 +14,16 @@ def display_grocery(request):
     print("in function")
     return render(request, "BetterCart/grocerylist.html", {"items":items})
 
-def add_grocery_item(request):
-    if request.method == 'POST':
-        item_name = request.POST['item_name']
-        new_item = GroceryItem(item_name=item_name)
-        new_item.save()
-        return redirect('grocery_list')
-    else:
-        return render(request, 'add_grocery_item.html')
+def addItemView(request):
+    print("in add item views")
+    return render(request, 'BetterCart/add_grocery_item.html')
+    # if request.method == 'POST':
+    #     item_name = request.POST['item_name']
+    #     new_item = GroceryItem(item_name=item_name)
+    #     new_item.save()
+    #     return redirect('grocery_list')
+    # else:
+    #     return render(request, 'add_grocery_item.html')
 
-
-# View function to remove a grocery item from the database
-def remove_grocery_item(request, item_id):
-    item_to_remove = GroceryItem.objects.get(id=item_id)
-    item_to_remove.delete()
-    return redirect('grocery_list')
+def finalizeListView(request):
+    print("in finalize List View")
