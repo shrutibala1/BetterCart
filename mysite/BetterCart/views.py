53901a1619fc7397f373aac2e24266318bb656ba
Hanyang Li <hl7zp@virginia.edu>
2023-03-25 15:54:49 -0400

    commit to push

--- mysite/BetterCart/views.py
+++ mysite/BetterCart/views.py
@@ -13,3 +13,19 @@ def display_grocery(request):
     items = GroceryItem.objects.all()
     print("in function")
     return render(request, "BetterCart/grocerylist.html", {"items":items})
+
+def add_grocery_item(request):
+    if request.method == 'POST':
+        item_name = request.POST['item_name']
+        new_item = GroceryItem(item_name=item_name)
+        new_item.save()
+        return redirect('grocery_list')
+    else:
+        return render(request, 'add_grocery_item.html')
+
+
+# View function to remove a grocery item from the database
+def remove_grocery_item(request, item_id):
+    item_to_remove = GroceryItem.objects.get(id=item_id)
+    item_to_remove.delete()
+    return redirect('grocery_list')
