d66e45083aa1c072734f72d5f234238721a45616
nikhitag79 <nguntu@gmail.com>
2023-03-25 15:52:42 -0400

    adding db to view

--- mysite/BetterCart/views.py
+++ mysite/BetterCart/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import GroceryItem
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def display_grocery(request):
+    items = GroceryItem.objects.all()
+    print("in function")
+    return render(request, "BetterCart/grocerylist.html", {"items":items})
