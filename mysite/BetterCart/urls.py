01eec458395ae5f07554beb418b80404bfacc783
nikhitag79 <nguntu@gmail.com>
2023-03-25 19:51:02 -0400

    changing urls to direct to home page

--- mysite/BetterCart/urls.py
+++ mysite/BetterCart/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('grocerylist/', views.display_grocery, name = 'display grocery'),
+    path('', views.display_grocery, name='index'),
+    # path('grocerylist/', views.display_grocery, name = 'display grocery'),
     path('add-item/', views.addItemView, name='add-item'),
     path('finalize-list/', views.finalizeListView, name='finalize-list'),
     path('add_grocery_filled_item/', views.addItemFilledView, name='add-item-filled'),
