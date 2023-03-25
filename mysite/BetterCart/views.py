2fb77713302a26cc8275905ee9e272202791d955
Hanyang Li <hl7zp@virginia.edu>
2023-03-25 15:02:41 -0400

    add and remove methods

--- mysite/BetterCart/views.py
+++ mysite/BetterCart/views.py
@@ -6,3 +6,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# Add a new grocery item to the database
+def add_grocery_item(item_name, index):
+    new_item = GroceryItem(item_name=item_name, index=index)
+    new_item.save()
+
+# Remove a grocery item from the database
+def remove_grocery_item(item_id):
+    item_to_remove = GroceryItem.objects.get(id=item_id)
+    item_to_remove.delete()
