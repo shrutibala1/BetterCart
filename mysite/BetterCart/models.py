fead6d2001c92b589b0b34e0a1e5a4acb1f3c8ca
nikhitag79 <nguntu@gmail.com>
2023-03-25 14:47:26 -0400

    added migrations

--- mysite/BetterCart/models.py
+++ mysite/BetterCart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class GroceryItem(models.Model):
+    item_name = models.CharField(max_length=255)
+    index = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
