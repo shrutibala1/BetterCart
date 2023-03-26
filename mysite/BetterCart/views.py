4f20f2157e9c81bc7842d90981a66199a1ddf7f6
Ethan Kacena-Merrell <gen7hz@virginia.edu>
2023-03-25 21:56:12 -0400

    dataset

--- mysite/BetterCart/views.py
+++ mysite/BetterCart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import GroceryItem
+from .models import GroceryItem, nutritionFacts
 import requests
 from bs4 import BeautifulSoup
 from .scraper import scrape_foodsubs
@@ -51,3 +51,4 @@ def ingredient_search(request):
         return render(request, 'BetterCart/ingredient_search_results.html', context)
     else:
         return render(request, 'BetterCart/ingredient_search_form.html')
+
